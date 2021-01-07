b16c3370b399e2cc32261ecbb89b97c907dbc3fb
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2021-01-07 20:39:06 +0300

    Client: Make server ip and port constant

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -8,13 +8,16 @@ import java.net.Socket;
 
 public class Client {
 
+    private static final String SERVER_IP = "192.168.1.67";
+    private static final int SERVER_PORT = 9000;
+
     private final Gson gson = new Gson();
 
     public void sendRequest(String identifier, Object body) {
         Request request = new Request(identifier, body);
 
         try {
-            Socket clientSocket = new Socket("192.168.1.67", 9000);
+            Socket clientSocket = new Socket(SERVER_IP, SERVER_PORT);
 
             // Send request
             String requestJson = gson.toJson(request);
@@ -25,7 +28,7 @@ public class Client {
             System.out.println("Response received by client " + responseJson);
 
             Response response = gson.fromJson(responseJson, Response.class);
-            System.out.println("Response received by client " + response.body);
+            System.out.println("Response body " + response.body);
 
             clientSocket.close();
         } catch (IOException e) {
