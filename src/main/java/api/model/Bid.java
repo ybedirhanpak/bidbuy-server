76c12752b5cda8a8d16f7ab7cc5fdfa41d87fcba
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2021-01-30 16:08:39 +0300

    Bid.java: Add createdDate field

--- src/main/java/api/model/Bid.java
+++ src/main/java/api/model/Bid.java
@@ -2,14 +2,18 @@ package api.model;
 
 import database.DatabaseModel;
 
+import java.util.Date;
+
 public class Bid extends DatabaseModel {
     public int fromUserId;
     public int toProductId;
     public double price;
+    public String createDate;
 
     public Bid(int fromUserId, int toProductId, double price) {
         this.fromUserId = fromUserId;
         this.toProductId = toProductId;
         this.price = price;
+        this.createDate = new Date().toString();
     }
 }
